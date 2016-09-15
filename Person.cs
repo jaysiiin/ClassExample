873f1d8901223b93bc023f19a4391aed94e8a589
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-09-15 13:16:43 -0400

    added two method

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample_Fall2016
+{
+    class Person
+    {
+
+        private int age;
+        private string name;
+        private string eyeColor;
+        public string greeting;
+
+        public void Greet()
+        {
+            Console.WriteLine(greeting);
+        }
+
+        private string Description()
+        {
+            string tempString;
+            tempString = "Hello I am " + name + " and I am " + age + " years old.";
+            return tempString;
+
+        }
+
+    }
+}
