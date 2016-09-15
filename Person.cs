e8c3257a25dd7bed0c6411c8589304ccfb8c267b
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-09-15 13:24:12 -0400

    added comments

--- Person.cs
+++ Person.cs
@@ -8,21 +8,36 @@ namespace ClassExample_Fall2016
 {
     class Person
     {
-
+        //fields
         private int age;
         private string name;
         private string eyeColor;
         public string greeting;
+        
 
-        public void Greet()
+        //properties
+
+
+        //constructors
+
+        //method
+
+
+        //This method takes a string input then prints out a greeting for a person
+        public void Greet(string name)
         {
-            Console.WriteLine(greeting);
+        //provides an output for this method 
+            Console.WriteLine("Hello" + name + ", " + greeting);
         }
 
+        //returns the users age and name in a description in a string
         private string Description()
         {
+            // the temporary string we're using to build our temp string
             string tempString;
-            tempString = "Hello I am " + name + " and I am " + age + " years old.";
+            tempString = "I am " + name + " and I am " + age + " years old.";
+
+            //reurning the results
             return tempString;
 
         }
